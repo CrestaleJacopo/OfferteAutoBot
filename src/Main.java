761097a8b9190a76ec3c10f56a173c403e0843df
JacopoCrestale <crestale064@gmail.com>
2024-12-20 21:25:08 +0100

    prepared for db, added links to scraper

--- src/Main.java
+++ src/Main.java
@@ -2,31 +2,38 @@ import org.telegram.telegrambots.longpolling.TelegramBotsLongPollingApplication;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.sql.SQLException;
 import java.util.Scanner;
 
 public class Main
 {
-    private static boolean botInit() {
+    private static void botInit() throws BotException{
         try {
             Scanner scanner = new Scanner(new File("token.txt"));
             String botToken = scanner.nextLine();
             TelegramBotsLongPollingApplication botsApplication = new TelegramBotsLongPollingApplication();
             botsApplication.registerBot(botToken, new OfferteAutoBot());
         } catch (TelegramApiException e) {
-            System.out.println("Errore api telegram");
-            return false;
+            System.out.println("Telegram API Error");
         } catch(FileNotFoundException e) {
-            System.out.println("Impossibile trovare il file del token");
-            return false;
+            System.out.println("Couldn't find bot token");
         } catch (Exception e) {
             e.printStackTrace();
-            return false;
         }
-        return true;
     }
+
     public static void main(String[] args)
     {
-        //botInit();
-        WebScraper.autoscout();
+        try {
+//            botInit();
+            String urlDatabase = "jdbc:mysql://localhost:3306/";
+            WebScraperDB scraperDB = new WebScraperDB(urlDatabase);
+            scraperDB.autoscout();
+//        } catch (BotException e) {
+//            System.out.println("Couldn't initialise bot");
+//            return;
+        } catch (SQLException e) {
+            System.out.println("Couldn't connect to db");
+        }
     }
 }
--- src/WebScraperDB.java
+++ src/WebScraperDB.java
@@ -2,14 +2,27 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
+import java.sql.Connection;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.HashMap;
 
-public class WebScraper
+public class WebScraperDB
 {
     private static int PAGE_LIMIT = 25; //max pages scraped for each site
     private static HashMap<String, String> ZIP_TO_PROVINCE;     //TODO: convert zip code to province name
-    public static void autoscout()
-    {
+    private Connection connection;
+
+    public WebScraperDB(String db_url) throws SQLException{
+//        try {
+//            connection = DriverManager.getConnection(db_url);
+//        } catch (SQLException e) {
+//            throw new SQLException();
+//        }
+    }
+
+    public void autoscout() {
         String url = "https://www.autoscout24.it/lst?offer=U&page=";
         try {
             for(int i = 1; i < PAGE_LIMIT; i++) {
@@ -25,17 +38,21 @@ public class WebScraper
                     String marca = article.attr("data-make").toLowerCase();
                     String modello = article.attr("data-model").toLowerCase();
                     Integer prezzo = Integer.parseInt(article.attr("data-price"));
+                    String link = "autoscout24.it" +
+                            article.select("div[class=\"ListItem_header__J6xlG ListItem_header_new_design__Rvyv_\"]").
+                            select("a[href]").attr("href");
                     Integer km = Integer.parseInt(article.attr("data-mileage"));
                     Integer anno = Integer.parseInt(article.attr("data-first-registration").split("-")[1]);   //va sistemato a solo anno
 
-//                    String provincia = article.attr("");    //DA SISTEMARE
+                    String provincia = article.attr("");    //DA SISTEMARE
                     String carburante = article.select("span[data-testid=\"VehicleDetails-gas_pump\"]").text().toLowerCase();
                     String cambio = article.select("span[data-testid=\"VehicleDetails-transmission\"]").text().toLowerCase();
-//                    System.out.println(marca + " " + modello + " " + prezzo + " " + km + " " + anno + " " + provincia + " " + carburante + " " + cambio); //DEBUG
+                    System.out.println(marca + " " + modello + " " + prezzo + " " + link + " " + km + " " + anno + " " + provincia + " " + carburante + " " + cambio); //DEBUG
                 }
             }
         } catch (Exception e) {
             System.out.println(e.toString());
         }
     }
+
 }
